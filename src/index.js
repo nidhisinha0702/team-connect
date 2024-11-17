53211ebe59e07da03629c6d4d73e07af3a07e4c2
Nidhi Kumari <nsinha7295@gmail.com>
2024-11-17 16:11:32 -0500

    adding comments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-
+// This is the main entry point of the application
 ReactDOM.render(
     <Router>
         <App />
